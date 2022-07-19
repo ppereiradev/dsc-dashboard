3715882810995468caed6c001bc5570fd5339937
Paulo Pereira <prpereiras89@gmail.com>
2022-07-18 21:57:47 -0300

    Fix restricted area

--- dsc_dashboard/dashboards/views.py
+++ dsc_dashboard/dashboards/views.py
@@ -2,36 +2,36 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-#@login_required
+@login_required
 def index(request):
     return render(request, 'dashboards/index.html')
 
-#@login_required
+@login_required
 def app_1(request):
     context ={'dashboard_name' : 'app_1' }
     return render(request, 'dashboards/index.html', context)
 
-#@login_required
+@login_required
 def app_3(request):
     context ={'dashboard_name' : 'app_3' }
     return render(request, 'dashboards/index.html', context)
 
-#@login_required
+@login_required
 def app_4(request):
     context ={'dashboard_name' : 'app_4' }
     return render(request, 'dashboards/index.html', context)
 
-#@login_required
+@login_required
 def app_5(request):
     context ={'dashboard_name' : 'app_5' }
     return render(request, 'dashboards/index.html', context)
 
-#@login_required
+@login_required
 def app_6(request):
     context ={'dashboard_name' : 'app_6' }
     return render(request, 'dashboards/index.html', context)
 
-#@login_required
+@login_required
 def app_7(request):
     context ={'dashboard_name' : 'app_7' }
     return render(request, 'dashboards/index.html', context)
