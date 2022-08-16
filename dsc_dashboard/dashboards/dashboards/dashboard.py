11ac96ec343c2ab90b6722efc215daa83b727b53
Paulo Pereira <prpereiras89@gmail.com>
2022-08-16 11:31:09 -0300

    Fix login redirection and preprocessing all data

--- dsc_dashboard/dashboards/dashboards/dashboard.py
+++ dsc_dashboard/dashboards/dashboards/dashboard.py
@@ -11,6 +11,8 @@ from .apps import app_1, app_2, app_3, app_4, app_5, app_6
 from data_updater.data_processing.processed_data import ProcessedData
 
 processed_data = ProcessedData()
+processed_data.get_processed_data_all()
+
 diretoria = processed_data.get_data_diretoria()
 conectividade = processed_data.get_data_conectividade()
 sistemas = processed_data.get_data_sistemas()
