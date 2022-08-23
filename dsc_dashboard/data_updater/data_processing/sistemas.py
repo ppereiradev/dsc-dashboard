dd14b07c1dc4af1781c78d9cd356c10d3b79762b
Paulo Pereira <prpereiras89@gmail.com>
2022-08-23 12:59:46 -0300

    Fix accumulated tickets in the sector sistemas

--- dsc_dashboard/data_updater/data_processing/sistemas.py
+++ dsc_dashboard/data_updater/data_processing/sistemas.py
@@ -40,40 +40,62 @@ class Sistemas(DataCleaning):
             super().clean_data()
             self.tickets = self.tickets[self.tickets['group'] == "Sistemas"]
 
-    def get_by_state(self):
+    def get_by_state(self, group=None):
         
         self.open_tickets_previous = self.closed_tickets_previous = self.closed_tickets_total = 0
         dates_three_months_ago_from_today = pd.period_range(pd.Timestamp.now().to_period('m')-3,freq='M', 
-                                                            periods=4).strftime('%Y-%m-%d').tolist()
+                                                                periods=4).strftime('%Y-%m-%d').tolist()
         last_day_three_months_ago = datetime.strptime(dates_three_months_ago_from_today[0] + " 23:59:59",
-                                                    '%Y-%m-%d %H:%M:%S').replace(day=1) - timedelta(days=1)
-
-        # getting keys from values
-        zammad_groups = [key for key,value in  ZAMMAD_GROUPS_TO_STD_SECTORS.items() if value == "Sistemas"]
-        for group in zammad_groups:
+                                                        '%Y-%m-%d %H:%M:%S').replace(day=1) - timedelta(days=1)
+        
+        if group:
             # getting old open tickets of Sistemas to calculate the acumulados
-            self.open_tickets_previous += count_tickets(
+            self.open_tickets_previous = count_tickets(
                 { "$and": [ 
                             {"group": group}, { "created_at": { "$lte": last_day_three_months_ago } } 
                     ] 
                 } )
-            self.closed_tickets_previous += count_tickets(
+            self.closed_tickets_previous = count_tickets(
                 { "$and": [ 
                             {"group": group}, { "close_at": { "$lte": last_day_three_months_ago } } 
                     ] 
                 })
-            self.closed_tickets_total += count_tickets(
+            self.closed_tickets_total = count_tickets(
                 { "$and": [ 
                             {"group": group}, { "state": "closed" } 
                     ] 
                 })
 
 
-        super().get_by_state(dates_three_months_ago_from_today, self.open_tickets_previous, self.closed_tickets_previous)
+            super().get_by_state(dates_three_months_ago_from_today, self.open_tickets_previous, self.closed_tickets_previous)
+
+        else:    
+            # getting keys from values
+            zammad_groups = [key for key,value in  ZAMMAD_GROUPS_TO_STD_SECTORS.items() if value == "Sistemas"]
+            for group_name in zammad_groups:
+                # getting old open tickets of Sistemas to calculate the acumulados
+                self.open_tickets_previous += count_tickets(
+                    { "$and": [ 
+                                {"group": group_name}, { "created_at": { "$lte": last_day_three_months_ago } } 
+                        ] 
+                    } )
+                self.closed_tickets_previous += count_tickets(
+                    { "$and": [ 
+                                {"group": group_name}, { "close_at": { "$lte": last_day_three_months_ago } } 
+                        ] 
+                    })
+                self.closed_tickets_total += count_tickets(
+                    { "$and": [ 
+                                {"group": group_name}, { "state": "closed" } 
+                        ] 
+                    })
+
+
+            super().get_by_state(dates_three_months_ago_from_today, self.open_tickets_previous, self.closed_tickets_previous)
 
     def get_processed_data(self, group=None):
         self.get_data_from_last_four_months()
         self.clean_data(group)
-        self.get_by_state()
+        self.get_by_state(group)
         self.get_leadtime()
         self.get_satisfaction()
